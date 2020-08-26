e4fd2c9a5f43838ead3d05f26f53115e85b9d6dd
BilbaoJ <69733287+BilbaoJ@users.noreply.github.com>
2020-08-26 16:57:04 -0500

    Actualización

--- tallerRecursividad/src/tallerrecursividad/Ejercicios.java
+++ tallerRecursividad/src/tallerrecursividad/Ejercicios.java
@@ -23,9 +23,9 @@ public class Ejercicios {
     }
       
     private int tribonacci(int n){
-        if (n == 0) {  
+        if(n == 0) {  
             return 0;
-        } else if (n == 1) {
+        } else if(n == 1) {
             return 0;
         }else if(n == 2){
             return 1;
@@ -62,23 +62,18 @@ public class Ejercicios {
             return false;
         }
     }
-    
+   
     public int numerologia(int n){
         
-        String num = String.valueOf(n);
-        
-        if(num.length() == 1){
+        if(n < 10){
             return n;
         }else{
-            return n%10 + numerologia(n/10);
-        }
+            int r = n%10 + n/10;
+            return numerologia(r);
+        } 
     }
     
-    
-    int  b;
-    int a;
-    char otro;
-    int resultado;
+    int  a, b, resultado;
     String c; 
     
     public String numerologia2(String n, int t){
@@ -98,8 +93,7 @@ public class Ejercicios {
         if (b == 1){
             return n;
         }else{
-            otro = num.charAt(0);
-            a = Character.getNumericValue(otro);
+            a = Character.getNumericValue(num.charAt(0));
             num = num.substring(1);
             b = num.length();
             n = Integer.parseInt(num);
--- tallerRecursividad/src/tallerrecursividad/TallerRecursividad.java
+++ tallerRecursividad/src/tallerrecursividad/TallerRecursividad.java
@@ -18,16 +18,19 @@ public class TallerRecursividad {
         
         
         Ejercicios r = new Ejercicios();
+        System.out.println("Secuencia Tribonacci");
         System.out.println(r.tribonacciPrincipal(11,0));
         int[] datos = new int[]{1,5,2,5,8,2,9};
         int t = datos.length - 1;
-        System.out.println(" ");
-        System.out.println(r.determinarSiExiste(datos,1,t));
-        System.out.println(r.verificarPalindromo("awsrtrtgr"));
-        System.out.println(r.numerologia(4864521));
+        System.out.println("");
+        System.out.println("El número está en el arreglo: " + r.determinarSiExiste(datos,1,t));
+        System.out.println("");
+        System.out.println("La palabra es palídroma: " + r.verificarPalindromo("awsrtrtgr"));
+        System.out.println("");
         String x = "4864521";
         int tam = x.length(); 
-        System.out.println(r.numerologia2(x, tam));
+        System.out.println("Resultado: " + r.numerologia2(x, tam));
+        System.out.println("Resultado: " + r.numerologia(4864521));
     }
     
 }
